000bc2d006398320a8ec02cd34988150a7fe0c7a
leonardnadeau <leonard.nadeau05@gmail.com>
2025-01-10 14:12:42 -0500

    i need to do the minus

--- calculator.py
+++ calculator.py
@@ -5,42 +5,107 @@ class Calculator:
         self.answer = None
         self.operation = None
         self.last_ans = None
-        self.entries = [None, None]
+        self.entry = None
+
+        self.window = tk.Tk()
+        self.window.geometry("400x500")
+        self.window.title("Calculator")
     
-    @property
-    def operation(self):
-        return self.operation
-    
-    @operation.setter
-    def operation(self, op):
-        self.operation = op
-        return self
-    
-    @property
-    def last_ans(self):
-        return self.last_ans
-    
-    @property
-    def entry_a(self):
-        return self.entries[0]
-    
-    @entry_a.setter
-    def entry_a(self, a):
-        self.entries[0] = a
-        return self
-    
-    @property
-    def entry_b(self):
-        return self.entries[1]
-    
-    @entry_b.setter
-    def entry_b(self, b):
-        self.entries[1] = b
-        return self
-    
-    def calc_reset(self):
+    def clear(self):
         self.__init__()
     
-    def compute(self):
-        return self.operation(self.entries[0],self.entries[1])
+    def run(self):
+        # self.create_widgets()
+        self.window.mainloop()
     
+    def computeParentheses(self, values):
+        while "(" in values:
+            x = values.index("(")
+            y = values.index(")")
+            values2 = values[x+1:y]
+            if "(" in values2:
+                self.computeParentheses(values2)
+            else:
+                values[x] = self.calculate(values2)
+                del values[x+1:y+1]
+        return values
+
+    def calculate(self, values):
+        while len(values) > 1:
+            while "^" in values:
+                z = values.index("^")
+                values[z-1] = values[z-1] ** values[z+1]
+                del values[z:z+2]
+            while "*" in values:
+                z = values.index("*")
+                values[z-1] = values[z-1] * values[z+1]
+                del values[z:z+2]
+            while "/" in values:
+                z = values.index("/")
+                values[z-1] = values[z-1] / values[z+1]
+                del values[z:z+2]
+            while "+" in values:
+                z = values.index("+")
+                values[z-1] = values[z-1] + values[z+1]
+                del values[z:z+2]
+            while "-" in values:
+                z = values.index("-")
+                values[z-1] = values[z-1] - values[z+1]
+                del values[z:z+2]
+        return values[0]
+    
+    def checkMinus(self, values):
+        while 
+
+    def separateValues(self, entry):
+        chars = entry.split()
+        values = []
+        for i in range(len(chars)):
+            if chars[i].isdigit():
+                for j in range(i+1, len(chars)):
+                    if chars[j].isdigit():
+                        continue
+                    else:
+                        values.append(float(chars[i:j])) if '.' in chars[i:j] else values.append(int(chars[i:j]))
+            elif chars[i] == '-':
+                if i == 0 or chars[i-1]:
+                    
+
+        '''
+        values = []
+        for i in range(len(entry)):
+            if i == len(entry)-1:
+                if entry[i].isdigit():
+                    values.append(int(entry[i]))
+                else:
+                    values.append(entry[i])
+            elif entry[i] == "-":
+                if i == 0 or not entry[i-1].isdigit():
+                    for j in range(i+1, len(entry)):
+                        if j == len(entry[i:])-1:
+                            if i == j-1:
+                                values.append(int(entry[j]) * -1)
+                            elif 
+
+            elif entry[i].isdigit():
+                for j in range(i+1, len(entry)):
+                    if j == len(entry)-1:
+                            values.append(float(entry[i:j])) if '.' in entry[i:j] else values.append(int(entry[i:j]))
+                    elif entry[j].isdigit() or entry[j] == '.':
+                        continue
+                    else:
+                        values.append(float(entry[i:j])) if '.' in entry[i:j] else values.append(int(entry[i:j]))
+                        i=j
+            else:
+                values.append(entry[i])
+        return values
+        '''
+
+    def compute(self, entry):
+        self.entry = entry
+        values = self.separateValues(self.entry)
+        no_paren = self.computeParentheses(values)
+        print(self.calculate(no_paren))
+
+calc = Calculator()
+calc.compute("-1")
